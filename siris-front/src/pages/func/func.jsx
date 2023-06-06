4cf52caa45f0eb364d613a13ee59b6a0ea33548c
João Batista <jbneto9@gmail.com>
2023-06-06 02:56:49 -0300

    Finalizada estrutura da pageFunc e botão de sair

--- siris-front/src/pages/func/func.jsx
+++ siris-front/src/pages/func/func.jsx
@@ -1,13 +1,42 @@
 import React from "react";
 import './func.css'
+import {BiCaretRight, BiSquare} from 'react-icons/bi'
+import { AuthProvider, useAuth } from '../../Auth/context'
+
 export const Funcionario = () => {
+    const login = useAuth();
+
+    const logout = () => login.logout();
+
     return (
-        <div id="pagefunc" className="boxfunc">
-            <button id='grafico1' type="submit">GRAFICO 1</button>
-            <button id='stop1' type="submit">PARAR 1</button>
-            <button id='grafico2' type="submit">GRAFICO 2</button>
-            <button id='stop2' type="submit">PARAR 2</button>
-            <button id='help' type="submit">HELP</button>
+        <div>
+            <div id="pagefunc" className="boxfunc">
+                <button type="submit" class='botao'>
+                <span><BiCaretRight/> GRÁFICO 1</span></button>
+                
+                <button type="submit" class='botaoStop'>
+                <span><BiSquare/> PARAR 1</span></button>
+                
+                <button type="submit" class='botao'>
+                <span><BiCaretRight/> GRÁFICO 2</span></button>
+                
+                <button type="submit" class='botaoStop'>
+                <span><BiSquare/> PARAR 2</span></button>
+                
+                <button id='help' type="submit" class='botao'>HELP</button>
+                
+                <button id='logoutfunc' type="submit" onClick={logout}>SAIR</button>
+            </div>
+            <div>
+
+                <div className="boxGrafico">
+                <span>GRÁFICO 1</span></div>
+
+                <div className="boxGrafico2">
+                <span>GRÁFICO 2</span></div>
+
+                <div id='cronometro' ></div>
+            </div>
         </div>
     )
 };
