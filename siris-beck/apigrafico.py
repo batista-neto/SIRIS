35c5862fda672727a77fd5a77f9f6bae7cdfe6a9
Vinicius Santos <santos.vinicius@discente.ufma.br>
2023-06-19 00:03:51 -0300

    grafico

---
+++ siris-beck/apigrafico.py
@@ -0,0 +1,54 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import random
+import time
+from datetime import datetime
+import threading
+
+app = Flask(__name__)
+CORS(app)  # Adiciona o middleware Flask-CORS
+
+data = []  # Lista para armazenar os dados
+times = []  # Lista para armazenar os tempos
+lock = threading.Lock()  # Lock para sincronização do acesso às listas
+update_thread = None  # Referência para a thread de atualização
+
+@app.route('/data', methods=['POST', 'GET'])
+def post_data():
+    try:
+        current_time = int(time.time())
+
+        # Obtém cópias dos dados e tempos atualizados
+        with lock:
+            updated_data = list(data)
+            updated_times = list(times)
+
+        return jsonify({'data': updated_data, 'times': updated_times, 'timestamp': current_time})
+
+    except Exception as e:
+        return jsonify({'message': 'Error occurred', 'error': str(e)})
+
+def update_data():
+    global data, times
+
+    start_time = datetime.now()  # Obtém o tempo de referência no início da atualização
+
+    for _ in range(20):  # Executa durante 20 segundos
+        value = round(random.uniform(0, 360), 2)  # Arredonda para duas casas decimais
+        current_time = int((datetime.now() - start_time).total_seconds())  # Calcula o tempo decorrido em segundos
+
+        with lock:
+            data.append(value)
+            times.append(current_time)
+
+        time.sleep(1)  # Aguarda 1 segundo
+
+    # Limpa os dados após 20 segundos
+    
+
+if __name__ == '__main__':
+    update_thread = threading.Thread(target=update_data)
+    update_thread.start()
+    app.run(port=4000)
+
+
--- siris-front/src/pages/func/grafico.js
+++ siris-front/src/pages/func/grafico.js
@@ -1,40 +1,52 @@
-import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Legend } from 'chart.js'
-import { Bar } from 'react-chartjs-2'
+import {
+  Chart as ChartJS,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import { Bar, Scatter } from 'react-chartjs-2'
 import React, { useState, useEffect } from 'react'
-ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Legend)
+import './func.css'
 
+
+ChartJS.register(LinearScale, PointElement, LineElement, Tooltip, Legend);
 
 function Barchart() {
     
-    const [chartData, setChartData] = useState({
-        datasets: [],
-    })
-    
-    const [chartOptions, setChartOptions] = useState({})
+    const chartOptions = {
+        maintainAspectRatio: false,
+        scales: {
+            y: {
+            beginAtZero: true,
+            },
+        },
+    };
 
-    useEffect(() => {
-        setChartData({
-            labels: ["jhon", "kevin", "geroge", "michael", "oreo"],
-            datasets: [{
-                label: "whom st let the dog out",
-                data: [12, 55, 34, 120, 720],
-                borderColor: "rgb(53, 162, 235)",
-                backgroundColor: "rgba(53, 162, 235, 0.4)",
-            }]
-        });
-        setChartOptions({
-            responsive: true,
-            plugins: {
-                legend: {
-                    position: "top"
-                },
-            }
-        });
-    });
-
+    function generateRandomData(length, minValue, maxValue) {
+        const data = [];
+        for (let i = 0; i < length; i++) {
+          const x = Math.random() * (maxValue - minValue) + minValue;
+          const y = Math.random() * (maxValue - minValue) + minValue;
+          data.push({ x, y });
+        }
+        return data;
+      }
+      
+      const chartData = {
+        datasets: [
+          {
+            label: 'A dataset',
+            data: generateRandomData(100, -100, 100),
+            backgroundColor: 'rgba(255, 99, 132, 1)',
+          },
+        ],
+      };
+      
     return(
-        <did>
-            <Bar className="grafico" options={chartOptions} data={chartData} />
+        <did className="grafico">
+            <Scatter className='grafico123' options={chartOptions} data={chartData} />
         </did>
     )
 }
